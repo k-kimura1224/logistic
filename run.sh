c783f8b2ac619d318585a6b4fe9c478aabea7c03
Keiji <kgeewimp@gmail.com>
2018-01-26 19:39:40 +0900

    add run.sh

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+cd exp_cplex4
+~/ILOG/CPLEX_Studio1263/cplex/bin/x86-64_linux/cplex < run.txt
+cd ..
+
+cd ug5/exp
+export OMP_NUMTHREADS=1
+../bin/fscip ../settings/large.set ../../data/musk2.logreg -q -sth 16 > ./musk2.log
+export OMP_NUMTHREADS=1
+../bin/fscip ../settings/noheur.set ../../data/musk2.logreg -q -sth 16 > ./musk2.log
+cd ../..
+
+
+
